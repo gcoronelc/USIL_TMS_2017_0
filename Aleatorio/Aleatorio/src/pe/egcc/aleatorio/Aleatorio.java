02590c4975a4e42a76360f428310a0732c185a1f
Gustavo Coronel <gcoronelc@gmail.com>
2017-02-09 10:45:48 -0500

    PRact 02

---
+++ Aleatorio/Aleatorio/src/pe/egcc/aleatorio/Aleatorio.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.egcc.aleatorio;
+
+import pe.egcc.aleatorio.view.MainView;
+
+/**
+ *
+ * @author firefox_usr
+ */
+public class Aleatorio {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    MainView.main(args);
+  }
+  
+}
